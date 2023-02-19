9eaaada9f74ce9375751dcf884a4024d86176d88
Joaquin Morales <joaquin.moralestr@gmail.com>
2023-02-19 14:28:56 -0300

    Basic cube with ar button

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { OrbitControls, Stars } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { ARButton, XR as Xr } from '@react-three/xr'
+import { useRef } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const Cube = () => {
+  const ref = useRef()
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <mesh ref={ref} position={[0, 0, 0]}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial attach='material' color='orange' />
+    </mesh>
+  )
+}
+
+function App () {
+  return (
+    <>
+      <ARButton />
+      <Canvas>
+        <Xr>
+          <Stars />
+          <ambientLight />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+          <Cube />
+          <OrbitControls />
+        </Xr>
+      </Canvas>
+    </>
   )
 }
 
