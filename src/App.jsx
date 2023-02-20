1336e651b656b276a431edf63f62cb632604c786
Joaquin Morales <joaquin.moralestr@gmail.com>
2023-02-19 21:06:28 -0300

    testing hit test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
-import { ARButton, Controllers, Interactive, XR as Xr } from '@react-three/xr'
+import { ARButton, Controllers, Interactive, useHitTest, XR as Xr } from '@react-three/xr'
 import { useRef } from 'react'
 
 const Cube = () => {
   const ref = useRef()
+
+  useHitTest((hit) => {
+    // use hitMatrix to position any object on the real life surface
+    hit.decompose(ref.current.position, ref.current.rotation, ref.current.scale)
+  })
+
   return (
     <mesh ref={ref} position={[0, 0, -5]}>
       <boxGeometry args={[1, 1, 1]} />
@@ -22,10 +28,10 @@ function App () {
           <Interactive>
             <ambientLight />
             <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-            <Controllers />
             <Cube />
-            <OrbitControls />
+            <Controllers />
           </Interactive>
+          <OrbitControls />
         </Xr>
       </Canvas>
     </>
